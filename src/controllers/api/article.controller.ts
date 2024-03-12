bd4fb91281d608da60b1cd289e3d6dade8a66b9c
Aleksandar Bursac <stevica.bursac.no.1@gmail.com>
2024-03-12 03:49:14 +0100

    Implementacija pravog error Response-a, error se pojavljuje sada na postman-u, a ne u konzoli

--- src/controllers/api/article.controller.ts
+++ src/controllers/api/article.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Query, UploadedFile, UseInterceptors } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put, Query, Req, UploadedFile, UseInterceptors } from "@nestjs/common";
 import { FileInterceptor } from "@nestjs/platform-express";
 import { Crud } from "@nestjsx/crud";
 import { Article } from "entities/article.entity";
@@ -79,23 +79,31 @@ export class ArticleController{
                 // 2. Provera tipa sadrzaja: image/jpeg, image/png(minetype)
 
                 if(!file.originalname.match(/\.(jpg|png)$/)){
-                    callback(new Error('Bad file extensions!'), false);
+                    req.fileFilterError = 'Bad file extension!';
+                    callback(null, false);
                     return;
                 }
                 if(!(file.mimetype.includes('jpeg') || file.mimetype.includes('png'))){
-                    callback(new Error('Bad file content!'), false);
+                    req.fileFilterError = 'Bad file content!';
+                    callback(null, false);
                     return;
                 }
                 callback(null, true);
             },
             limits: {
                 files: 1,
-                fieldSize: StorageConfig.photoMaxFileSize,
+                fileSize: StorageConfig.photoMaxFileSize,
             },
 
         })
     )
-    async uploadPhoto(@Param('id') articleId: number, @UploadedFile() photo): Promise <ApiResponse | Photo>{
+    async uploadPhoto(@Param('id') articleId: number, @UploadedFile() photo, @Req() req): Promise <ApiResponse | Photo>{
+        if(req.fileFilterError){
+            return new ApiResponse('error', -4002, req.fileFilterError);
+        }
+        if(!photo){
+            return new ApiResponse('error', -4002, 'File not uploaded!');
+        }
        
         const newPhoto: Photo = new Photo();
         newPhoto.articleId = articleId;
