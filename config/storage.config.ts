cc9e7f42a149ae17e0cedcb8753d01040ee3e22a
Aleksandar Bursac <stevica.bursac.no.1@gmail.com>
2024-03-19 17:05:32 +0100

    Implementacija staticne slike iz foldera storage!

--- config/storage.config.ts
+++ config/storage.config.ts
@@ -3,6 +3,8 @@ export const StorageConfig = {
 
     photo:{
         destination: '../storage/photos/',
+        urlPrefix: '/assets/photos',
+        maxAge: 1000 * 60 * 60 *24 * 7, // 7 dana
         maxSize: 3 * 1024 * 1024,
         resize: {
             thumb: {
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { StorageConfig } from 'config/storage.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(StorageConfig.photo.destination, {
+    prefix: StorageConfig.photo.urlPrefix,
+    maxAge: StorageConfig.photo.maxAge, //  7 dana
+    index: false,
+
+  });
   
   const config = new DocumentBuilder()
     .setTitle('Your API Title')
